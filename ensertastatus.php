7f63fa5b8be626a6d2ab4f01434ea6370b843577
gilberto <18030244@itesa.edu.mx>
2022-10-20 20:57:03 -0500

    diseño

--- ensertastatus.php
+++ ensertastatus.php
@@ -8,3 +8,11 @@ $combustible=$_POST['combustible'];
 require_once("/CRUD/class/Consultas.php");
 $usuarios = Usuarios::singleton();
 $usuarios->insertarstatu($camion,$ruta,$latitud,$longitud,$velocidad,$combustible);
+$data=$usuarios->Consulta();
+if (count($data) > 0) {
+    foreach ($data as $fila) {
+        $cuen = $fila["Id_ruta"];
+        break;
+    }
+    echo $cuen;
+}
--- inserta.php
+++ inserta.php
@@ -1,12 +1,9 @@
 <?php
-
 $placas=$_POST['placas'];
 $tipo =$_POST['tipo'];
 $capacidad=$_POST['capacidad'];
 $ruta=$_POST['ruta'];
-echo "ganar";
 require_once("./CRUD/class/Consultas.php");
 $usuarios = Usuarios::singleton();
 $data = $usuarios->insertarcam($placas,$tipo,$capacidad,$ruta);
-echo $data;
 ?>
